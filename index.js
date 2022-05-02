f4d2a40f9a6810747e0e09dcf5b8995d32a6715f
Anastasiya Klyshko <new-emerald@yandex.ru>
2022-05-02 19:55:27 +1000

    add prodject

---
+++ index.js
@@ -0,0 +1,28 @@
+function showMessage () {
+let form = prompt ('Как тебя зовут?');
+alert (`Привет, ${form}!`);
+}
+
+function calcAdd () {
+let a = prompt ('Первое число:');
+let b = prompt ('Второе число:');
+alert (Number(a)+Number(b));
+}
+
+function calcSub () {
+let a = prompt ('Первое число:');
+let b = prompt ('Второе число:');
+alert (Number(a)-Number(b));
+}
+
+function calcMulti () {
+let a = prompt ('Первое число:');
+let b = prompt ('Второе число:');
+alert (Number(a)*Number(b));
+}
+
+function calcDiv () {
+let a = prompt ('Первое число:');
+let b = prompt ('Второе число:');
+alert (Number(a)/Number(b));
+}
